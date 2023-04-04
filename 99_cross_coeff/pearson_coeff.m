becb1a4583f98e3e7de2dbdaf5eeba904b9cfb85
ben <benedic.lee@gmail.com>
2023-04-03 18:00:18 -0700

    update

--- 99_cross_coeff/pearson_coeff.m
+++ 99_cross_coeff/pearson_coeff.m
@@ -1,7 +1,7 @@
 warning off; clear all; close all; clc;
 %% Import video feature
 observe = 35 * 30; % time * fame
-num_db = 14;
+num_db = 17;
 for idx=1:num_db
     clear A R;
     % idx = 1;
@@ -10,24 +10,31 @@ for idx=1:num_db
     clear X;
     %load('video/a1_keyframes.mat');
     
-    %% import Audio feature
+    %% import Audio feature: spectrum
     load(sprintf('feat/35s/a%d_spectrum.mat',idx));
     fa = ssq;
     fa_s = sum(ssq);
     clear ssq;
-    
+
+    %% import Audio feature: pitch
+    load(sprintf('feat/35s/a%d_pitch.mat',idx));
+    f0 = f0';
     %% import Interface feature
     % fi
     stack_v(idx,:) = fv(:);
     stack_a(idx,:) = fa_s(:);
+    stack_f(idx,:) = f0(:);
     
     %% Compute correlations
   
     rnd = rand(1,observe);    
-    A = [fv' fa_s'];
+%     A = [fv' fa_s'];
+    A = [fv' f0'];
 %     A = [fv(1:observe)' fa_s(1:observe)' ];
-    R = corrcoef(A);
+%     R = corrcoef(A);
+    [R, alp] = corr(A,'Type','Spearman');
     Rs(idx) = R(2,1);
+    Alps(idx) = alp(2,1);
 end
 figure(1);
 stem(Rs);
@@ -35,16 +42,39 @@ stem(Rs);
 
 for i= 1:num_db
     for j= 1:num_db
-         tmp = corrcoef([stack_v(i,:)' , stack_a(j,:)']);
+%          tmp = corrcoef([stack_v(i,:)' , stack_a(j,:)']);
+         [tmp, alpc] = corr([stack_v(i,:)' , stack_a(j,:)'],'Type','Spearman');
+%          tmp = corrcoef([stack_v(i,:)' , stack_f(j,:)']);
+%          [tmp, alpc] = corr([stack_v(i,:)' , stack_f(j,:)'],'Type','Spearman');
          xA(i,j) = tmp(2,1);       
+         xAlpha(i,j) = alpc(2,1);
     end
 end
-
+% figure(2);
+% heatmap(xA);
+% title('Unsupervised Crosscorrelation Mapping'); 
+% xlabel('Audio: Spectral Power'); % 33ms
+% ylabel('Video: Interframe histogram difference');  % 30fps =  33ms
+figure(2);
 heatmap(xA);
+title('Unsupervised Spearman Crosscorrelation Mapping'); 
+xlabel('Audio: Spectral Power'); % 33ms
+ylabel('Video: Interframe histogram difference');  % 30fps =  33ms
+
+figure(3);
+heatmap(xAlpha);
+title('Unsupervised Spearman Crosscorrelation Alpha Mapping'); 
+xlabel('Audio: Spectral Power'); % 33ms
+ylabel('Video: Interframe histogram difference');  % 30fps =  33ms
+
+% figure(4);
+% [cmd, eigvals] = cmdscale(A);
+% plot(Y(:,1),Y(:,2),'.');
+
+
+
 % y - video
 % x - audio
-audio 1 video 2
-audio 2 video 1
 
 % figure(2)
 % for idx=1:num_db
